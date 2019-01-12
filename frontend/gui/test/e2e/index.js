bad03f7f2e80c5c18d239bfc953446e43f3229bd
gipsond <david.gipson@utexas.edu>
2019-01-11 20:19:39 -0600

    [gui] set up basic 'should' tests

--- frontend/gui/test/e2e/index.js
+++ frontend/gui/test/e2e/index.js
@@ -11,7 +11,7 @@ require('babel-register')({
 // Attach Chai APIs to global scope
 const { expect, should, assert } = require('chai')
 global.expect = expect
-global.should = should
+global.should = should()
 global.assert = assert
 
 // Require all JS files in `./specs` for Mocha to consume
--- frontend/gui/test/e2e/specs/Launch.spec.js
+++ frontend/gui/test/e2e/specs/Launch.spec.js
@@ -4,10 +4,17 @@ describe('Launch', function () {
   beforeEach(utils.beforeEach)
   afterEach(utils.afterEach)
 
+  it('shows a single initial window', function () {
+    return this.app.client.getWindowCount()
+      .then(count => {
+        count.should.equal(1)
+      })
+  })
+
   it('shows the proper application title', function () {
     return this.app.client.getTitle()
       .then(title => {
-        expect(title).to.equal('lc3tools')
+        title.should.equal('lc3tools')
       })
   })
 })
