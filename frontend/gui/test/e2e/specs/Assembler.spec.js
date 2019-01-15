1ba8d80507f28f289c91f1b32fa56d4b813bfca2
gipsond <david.gipson@utexas.edu>
2019-01-15 00:59:54 -0600

    [gui] Added 'long program' assembler test

--- frontend/gui/test/e2e/specs/Assembler.spec.js
+++ frontend/gui/test/e2e/specs/Assembler.spec.js
@@ -23,6 +23,7 @@ could not open  for reading\
     })
 
     describe('saves and assembles', function () {
+
         const successMessage = '\
 <span class="text-bold">\
 <span class="text-green">\
@@ -44,7 +45,7 @@ assembly successful\
 </span>\
 <br>'
         
-        const saveAndAssemble = function (client, lines) {
+        const saveAssembleAndAssertSuccess = function (client, lines) {
             // Start testing command by clicking text editor
             var command = client.click('div.ace_content')
 
@@ -70,7 +71,7 @@ assembly successful\
         }
 
         it('the minimal LC-3 program', function () {
-            return saveAndAssemble(this.app.client,
+            return saveAssembleAndAssertSuccess(this.app.client,
                 ['.ORIG x3000'
                 , 'HALT'
                 , '.END'
@@ -78,13 +79,68 @@ assembly successful\
         )})
 
         it('an addition LC-3 program', function () {
-            return saveAndAssemble(this.app.client,
-                ['.ORIG x3000'
+            return saveAssembleAndAssertSuccess(this.app.client,
+                ['.ORIG x4000'
                 , 'ADD R0, R0, R0'
                 , 'HALT'
                 , '.END'
                 ]
         )})
+
+        it('the "Hello, World!" LC-3 program', function () {
+            return saveAssembleAndAssertSuccess(this.app.client,
+                ['.ORIG x3000'
+                , 'LEA R0, GREETING'
+                , 'PUTS'
+                , 'HALT'
+                , 'GREETING .STRINGZ "Hello, World!"'
+                , '.END'
+                ]
+            )
+        })
+
+        it('a long, formatted LC-3 program', function () {
+            this.timeout(5000)
+            return saveAssembleAndAssertSuccess(this.app.client,
+                ['\t.ORIG x3000' 
+                , 'Back space'
+                , '; Check Palindrome'
+                , '; This program checks if the string at PTR is a palindrome.'
+                , '\tLEA R0, PTR'
+                , 'ADD R1, R0, #0'
+                , 'Back space'
+                , 'AGAIN LDR R2, R1, #0'
+                , '\tBRz CONT'
+                , 'ADD R1, R1, #1'
+                , 'BRnzp AGAIN'
+                , 'Back space'
+                , 'CONT  ADD R1, R1, #-1'
+                , 'Back space'
+                , 'LOOP  LDR R3, R0, #0 ; Main Loop'
+                , '\tLDR R4, R1, #0'
+                , 'NOT R4, R4'
+                , 'ADD R4, R4, #1'
+                , 'ADD R3, R3, R4'
+                , 'BRnp NO'
+                , 'ADD R0, R0, #1'
+                , 'ADD R1, R1, #-1'
+                , 'NOT R2, R0'
+                , 'ADD R2, R2, #1'
+                , 'ADD R2, R1, R2'
+                , 'BRnz YES'
+                , 'BR LOOP'
+                , 'Back space'
+                , 'YES   AND R5, R5, #0'
+                , '\tADD   R5, R5, #1'
+                , 'BRnzp DONE'
+                , 'Back space'
+                , 'NO    AND R5, R5, #0'
+                , 'DONE  HALT'
+                , 'PTR   .FILL X4000'
+                , '\t.END'
+                ]
+            )
+        })
     })
     
 
